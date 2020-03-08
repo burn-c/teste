8bb16de458e2d91bf7c44becf128012d3221bed0
burn-c <chopcsom@gmail.com>
2020-03-08 18:13:15 -0300

    Config: Mock useSelector

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,42 +1,57 @@
 import React from 'react';
-
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { useSelector } from 'react-redux';
+import { render } from '@testing-library/react';
 
 import TechList from '~/components/TechList';
 
+jest.mock('react-redux');
+
 describe('TechList component', () => {
-    beforeEach(() => {
-        localStorage.clear();
-    })
+    it('should render tech list', () => {
+        useSelector.mockImplementation(cb => cb({
+            techs: ['Node.js', 'ReactJS']
+        }));
 
-    it('should be able to add new tech', () => {
-        const { getByText, getByTestId, getByLabelText } = render(<TechList />)
+        const { getByTestId, getByText, debug } = render(<TechList />);
 
-        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' }});
-        fireEvent.submit(getByTestId('tech-form'));
+        debug();
 
-        
         expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-        expect(getByLabelText('Tech')).toHaveValue('');
+        expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
     });
 
-    it('should store techs in storage', () => {
-        let { getByTestId, getByLabelText, getByText } = render(<TechList />)
+    // beforeEach(() => {
+    //     localStorage.clear();
+    // })
 
-        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' }});
-        fireEvent.submit(getByTestId('tech-form'));
+    // it('should be able to add new tech', () => {
+    //     const { getByText, getByTestId, getByLabelText } = render(<TechList />)
+
+    //     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' }});
+    //     fireEvent.submit(getByTestId('tech-form'));
 
         
-        expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-        expect(getByLabelText('Tech')).toHaveValue('');
+    //     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    //     expect(getByLabelText('Tech')).toHaveValue('');
+    // });
 
-        cleanup();
+    // it('should store techs in storage', () => {
+    //     let { getByTestId, getByLabelText, getByText } = render(<TechList />)
 
-        ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+    //     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' }});
+    //     fireEvent.submit(getByTestId('tech-form'));
 
-        expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
-        expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-    });
+        
+    //     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    //     expect(getByLabelText('Tech')).toHaveValue('');
+
+    //     cleanup();
+
+    //     ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+
+    //     expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
+    //     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    // });
 
 
 });
